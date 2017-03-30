540c60ccb9713e5b09fa1166cfc44d68a86337cb
Martin Verbeek <verbeek_martin@hotmail.com>
2017-03-30 19:29:57 +0200

    V 3.12 Implement first go, also need V3.12 of the DomoticzServer app

---
+++ devicetypes/verbem/domoticzsensor.src/domoticzsensor.groovy
@@ -0,0 +1,105 @@
+/**
+ *  domoticzSensor
+ *
+ *  Copyright 2016 Martin Verbeek
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "domoticzSensor", namespace: "verbem", author: "SmartThings") {
+		capability "Temperature Measurement"
+		capability "Sensor"
+		capability "Battery"
+		capability "Actuator"
+		capability "Refresh"
+        capability "Signal Strength"
+        capability "Relative Humidity Measurement"
+        
+        attribute "pressure", "number"
+        }
+
+	tiles(scale: 2) {
+        
+		valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
+			state "battery", label:'${currentValue}% battery', unit:""
+		}
+        
+		valueTile("temperature", "device.temperature", decoration: "flat", inactiveLabel: false, width: 2, height: 2, canChangeIcon: true) {
+			state "temparature", label:'${currentValue} C', unit:"C"
+		}
+
+		valueTile("humidity", "device.humidity", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
+			state "humidity", label:'${currentValue}% humidity', unit:"%"
+		}
+
+		valueTile("pressure", "device.pressure", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
+			state "pressure", label:'${currentValue} HPa', unit:"HPa"
+		}
+
+		valueTile("rssi", "device.rssi", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "rssi", label:'Signal ${currentValue}', unit:""
+        }
+
+		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+
+		main "temperature"
+		details(["temperature", "humidity", "pressure", "battery", "rssi", "refresh"])
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'motion' attribute
+
+}
+
+def refresh() {
+	log.debug "Executing 'refresh'"
+
+    if (parent) {
+        parent.domoticz_poll(getIDXAddress())
+    }
+
+}
+
+
+// gets the IDX address of the device
+private getIDXAddress() {
+	
+    def idx = getDataValue("idx")
+        
+    if (!idx) {
+        def parts = device.deviceNetworkId.split(":")
+        if (parts.length == 3) {
+            idx = parts[2]
+        } else {
+            log.warn "Can't figure out idx for device: ${device.id}"
+        }
+    }
+
+    //log.debug "Using IDX: $idx for device: ${device.id}"
+    return idx
+}
+
+/*----------------------------------------------------*/
+/*			execute event can be called from the service manager!!!
+/*----------------------------------------------------*/
+def generateEvent (Map results) {
+    results.each { name, value ->
+    	def v = value         
+        log.info "generateEvent " + name + " " + v
+        sendEvent(name:"${name}", value:"${v}")
+        }
+        return null
+}
