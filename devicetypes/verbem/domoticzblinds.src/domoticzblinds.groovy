642b3aaedbd3cf68488c1811f7a6ceae4e21f7a8
Martin Verbeek <verbeek_martin@hotmail.com>
2018-02-13 19:55:44 +0100

    V4.00 windowShade capability and childdevice domoticzBlindsSmart

--- devicetypes/verbem/domoticzblinds.src/domoticzblinds.groovy
+++ devicetypes/verbem/domoticzblinds.src/domoticzblinds.groovy
@@ -17,6 +17,7 @@
  *	3.0 2016-12-24 cleanup of DTH statuses
  *  3.1 2017-05-10 Adding end of day scheduling for blinds as an offset to sunset, these are set in the Smart Screens app. 
  *  3.2 2017-07-12 Adding HC and parent check
+ *	4.0 2018-02-12 Add windowShade capability
  */
 import groovy.time.TimeCategory 
 import groovy.time.TimeDuration
@@ -36,6 +37,7 @@ metadata {
         capability "Door Control"
         capability "Signal Strength"
 		capability "Health Check"
+        capability "Window Shade"
 
         // custom attributes
         attribute "networkId", "string"
@@ -56,8 +58,8 @@ metadata {
 
         // custom commands
         command "parse"     // (String "<attribute>:<value>[,<attribute>:<value>]")
-        command "close"
-        command "open"
+        //command "close"
+        //command "open"
         command "stop"
         command "setLevel"
         command "calibrate"
@@ -68,15 +70,13 @@ metadata {
 
     tiles (scale: 2) {
 	    multiAttributeTile(name:"richDomoticzBlind", type:"generic",  width:6, height:4, canChangeIcon: true, canChangeBackground: true) {
-        	tileAttribute("device.switch", key: "PRIMARY_CONTROL") {
-                attributeState "Open", 			label:"   Open   ", backgroundColor:"#e86d13", nextState:"Going Down", action:"close"
-                attributeState "Going Up", 		label:" Going Up ", backgroundColor:"#e86d13", nextState:"Going Down"
-
-				attributeState "Stopped", 		label:" Stopped  ", backgroundColor:"#11A81C", action:"open"
-                
-                attributeState "Closed", 		label:"  Closed  ",  backgroundColor:"#00a0dc", nextState:"Going Up", action:"open"
-                attributeState "Going Down", 	label:"Going Down",  backgroundColor:"#00a0dc", nextState:"Going Up"
-				attributeState "Error", 		label:"Install Error", backgroundColor: "#bc2323"
+        	tileAttribute("device.windowShade", key: "PRIMARY_CONTROL") {
+                attributeState "open", 				label:"Open", 			backgroundColor:"#e86d13", nextState:"closing", action:"close"
+                attributeState "opening", 			label:"Opening", 		backgroundColor:"#e86d13", nextState:""
+				attributeState "partially open", 	label:"Partly Open", 	backgroundColor:"#11A81C", action:"open"               
+                attributeState "closed", 			label:"Closed",  		backgroundColor:"#00a0dc", nextState:"opening", action:"open"
+                attributeState "closing", 			label:"Closing",  		backgroundColor:"#00a0dc", nextState:""
+				attributeState "Error", 			label:"Install Error", 	backgroundColor:"#bc2323"
             }
             tileAttribute("device.level", key: "SLIDER_CONTROL", range:"0..16") {
             	attributeState "level", action:"setLevel" 
@@ -103,7 +103,7 @@ metadata {
             state "default", label:'Calibrate', icon:"st.doors.garage.garage-closing",
                 action:"calibrate"
         }
-
+/*
  		standardTile("windBearing", "device.windBearing",  inactiveLabel: false, width: 2, height: 2, decoration:"flat") {
 			state "windBearing", label:'${currentValue}', unit:"", icon:"https://raw.githubusercontent.com/verbem/SmartThingsPublic/master/devicetypes/verbem/domoticzblinds.src/windBearing.png"            
         }
@@ -136,14 +136,6 @@ metadata {
 			state "cloudCover", label:'${currentValue}%', unit:"%", icon:"https://raw.githubusercontent.com/verbem/SmartThingsPublic/master/devicetypes/verbem/domoticzblinds.src/cloudCover.png"            
         }
 		
-		standardTile("rssi", "device.rssi", inactiveLabel: false, width: 2, height: 2, decoration:"flat") {
-			state "rssi", label:'Signal ${currentValue}', unit:"", icon:"https://raw.githubusercontent.com/verbem/SmartThingsPublic/master/devicetypes/verbem/domoticzsensor.src/network-signal.png"
-		}
-        
-		standardTile("somfy", "device.somfySupported", inactiveLabel: false, width: 2, height: 2, decoration:"flat") {
-			state "somfySupported", label:'${currentValue}', unit:"", icon:"https://raw.githubusercontent.com/verbem/SmartThingsPublic/master/devicetypes/verbem/domoticzblinds.src/Somfy.png"
-		}
-        
 		standardTile("eodAction", "device.eodAction", inactiveLabel: false, width: 2, height: 2, decoration:"flat") {
 			state "eodAction", label:'${currentValue} EoD Action', unit:""
 		}
@@ -155,13 +147,26 @@ metadata {
 		standardTile("eodDone", "device.eodDone", inactiveLabel: false, width: 2, height: 2, decoration:"flat") {
 			state "eodDone", label:'${currentValue}', unit:""
 		}
-
+*/
         standardTile("Refresh", "device.refresh", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
             state "refresh", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
         }
-
+        
+		standardTile("rssi", "device.rssi", inactiveLabel: false, width: 2, height: 2, decoration:"flat") {
+			state "rssi", label:'Signal ${currentValue}', unit:"", icon:"https://raw.githubusercontent.com/verbem/SmartThingsPublic/master/devicetypes/verbem/domoticzsensor.src/network-signal.png"
+		}
+        
+		standardTile("somfy", "device.somfySupported", inactiveLabel: false, width: 2, height: 2, decoration:"flat") {
+			state "somfySupported", label:'${currentValue}', unit:"", icon:"https://raw.githubusercontent.com/verbem/SmartThingsPublic/master/devicetypes/verbem/domoticzblinds.src/Somfy.png"
+		}
+        
+		childDeviceTile("windBearing", "SmartScreens", decoration: "flat", width: 2, height: 2, childTileName: "windBearing")   
+        childDeviceTile("windSpeed", "SmartScreens", decoration: "flat", width: 2, height: 2, childTileName: "windSpeed")
+        childDeviceTile("sunBearing", "SmartScreens", decoration: "flat", width: 2, height: 2, childTileName: "sunBearing")
+        childDeviceTile("cloudCover", "SmartScreens", decoration: "flat", width: 2, height: 2, childTileName: "cloudCover")
+        
         main(["richDomoticzBlind"])
-        details(["richDomoticzBlind", "Up", "Stop", "Down", "Cal", "rssi", "somfy", "windBearing", "windSpeed", "sunBearing", "cloudCover", "Refresh"])
+	    details(["richDomoticzBlind", "Up", "Stop", "Down", "Cal", "rssi", "Refresh", "windBearing", "windSpeed", "sunBearing", "cloudCover"])
 
     }    
 }
@@ -196,7 +201,8 @@ def parse(String message) {
 def on() {
 	log.debug "Close()"
     if (parent) {
-        parent.domoticz_on(getIDXAddress())
+		sendEvent(name:'windowShade', value:"closed" as String)
+		parent.domoticz_on(getIDXAddress())
     }
 
 }
@@ -204,6 +210,7 @@ def on() {
 def off() {
 	log.debug "Open()"
     if (parent) {
+		sendEvent(name:'windowShade', value:"open" as String)
         parent.domoticz_off(getIDXAddress())
     }
 }
@@ -211,6 +218,7 @@ def off() {
 def close() {
 	log.debug "Close()"
     if (parent) {
+		sendEvent(name:'windowShade', value:"closed" as String)
         parent.domoticz_on(getIDXAddress())
     }
 }
@@ -232,6 +240,7 @@ def poll() {
 def open() {
 	log.debug "Open()"
     if (parent) {
+		sendEvent(name:'windowShade', value:"open" as String)
         parent.domoticz_off(getIDXAddress())
     }
 }
@@ -240,6 +249,15 @@ def stop() {
 	log.debug "Stop()"
     if (parent) {
         sendEvent(name:'switch', value:"Stopped" as String)
+        presetPosition()
+    }
+
+}
+
+def presetPosition() {
+	log.debug "presetPosition()"
+    if (parent) {
+        sendEvent(name:'windowShade', value:"partially open" as String)
         parent.domoticz_stop(getIDXAddress())
     }
 
@@ -277,6 +295,7 @@ def setLevel(level) {
         }
         else {
             sendEvent(name:'switch', value:"Stopped" as String)
+            sendEvent(name:'windowShade', value:"partially open" as String)
             parent.domoticz_stop(getIDXAddress())
         }
     }
@@ -328,10 +347,25 @@ def calibrate() {
 }
 
 def eodRunOnce(tempTime) {
+	createComponent()
+    
+    def tempEodAction = device.currentValue("eodAction")
+    runOnce(tempTime, handlerEod, [overwrite: false, data: ["eodAction": tempEodAction]])
 
-def tempEodAction = device.currentValue("eodAction")
-runOnce(tempTime, handlerEod, [overwrite: false, data: ["eodAction": tempEodAction]])
+}
 
+private def createComponent() {
+	// this will be first call Smart Screens does to the device. If no component child exists, create it now.
+    log.info "CreateComponents"
+    def children = getChildDevices()
+    if (!children) {
+    	try {
+        	addChildDevice("verbem", "domoticzBlindsSmart", "${device.deviceNetworkId}-SmartScreen", null,
+                            [completedSetup: true, label: "${device.displayName} (SmartScreens)",
+                             isComponent: true, componentName: "SmartScreens", componentLabel: "SmartScreens"])
+            }
+        catch (e) {log.error e}
+    }
 }
 
 // gets the IDX address of the device
@@ -356,7 +390,15 @@ private getIDXAddress() {
 /*			execute event can be called from the service manager!!!
 /*----------------------------------------------------*/
 def generateEvent (Map results) {
+    def children = getChildDevices()
+    if (children) {
+    	children.each { dev ->
+            def passedResults = results
+	    	dev.generateEvent(passedResults)
+     	}
+    }
 
+return
 results.each { name, value ->
 	log.info "generateEvent " + name + " " + value
     
@@ -409,6 +451,7 @@ def initialize() {
 
 	if (parent) {
         sendEvent(name: "DeviceWatch-Enroll", value: groovy.json.JsonOutput.toJson([protocol: "LAN", scheme:"untracked"]), displayed: false)
+        createComponent()
     }
     else {
     	log.error "You cannot use this DTH without the related SmartAPP Domoticz Server, the device needs to be a child of this App"
